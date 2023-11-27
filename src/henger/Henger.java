ebc5a399898dd8c4d2021120150fe3d3f4af46df
DVMaja <majadreilinger@gmail.com>
2023-11-27 08:42:33 +0100

    komment kiszedve

--- src/henger/Henger.java
+++ src/henger/Henger.java
@@ -24,7 +24,7 @@ public class Henger {
     }
 
     public double terfogat() {        
-        return Math.pow(sugar, 2)*3.14*magassag;//r2 pi * magasság
+        return Math.pow(sugar, 2)*3.14*magassag;
     }
 
     @Override
