ae3429575d98669edd7d59820342bcc6bdbafe3e
DVMaja <majadreilinger@gmail.com>
2023-11-29 13:08:47 +0100

    Gomb megvan

--- src/faidom/Gomb.java
+++ src/faidom/Gomb.java
@@ -1,6 +1,21 @@
-
 package faidom;
 
-public class Gomb {
-    
+public class Gomb extends FaIdom {
+
+    private double sugar;
+
+    public Gomb(double sugar) {
+        this.sugar = sugar;
+    }
+
+    @Override
+    public double terfogat() {
+        return (4 * Math.pow(sugar, 3) * Math.PI) / 3;
+    }
+
+    @Override
+    public String toString() {
+        return "Gömb{" + "sugár= " + sugar + " térfogat= " + terfogat() + "súly= " + suly() + '}';
+    }
+
 }
