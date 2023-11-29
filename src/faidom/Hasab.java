7c15619ea408c979d430b46ee43abdf70687295b
DVMaja <majadreilinger@gmail.com>
2023-11-29 13:14:42 +0100

    Hasab megvan

--- src/faidom/Hasab.java
+++ src/faidom/Hasab.java
@@ -1,5 +1,25 @@
 package faidom;
 
-public class Hasab {
+public class Hasab extends FaIdom {
+
+    private double a, b, magassag;
+
+    public Hasab(double a, double b, double magassag) {
+        this.a = a;
+        this.b = b;
+        this.magassag = magassag;
+    }
+
+    @Override
+    public double terfogat() {
+        double t1 = a * b;
+        return t1 * magassag;
+    }
+
+    @Override
+    public String toString() {
+        return "Hasáb{" + "a= " + a + "b= " + b + "magasság= " + magassag
+                + "\ntérfogat= " + terfogat() + "súly= " + suly() + '}';
+    }
 
 }
